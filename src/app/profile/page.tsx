32c4f087744aee4a2a1f26a35a05b926034e8f6e
Rahul Bishwokarma <rlbishwokarma@gmail.com>
2023-07-03 17:25:10 +0545

    middleware added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Toaster, toast } from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = React.useState("");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster />
       <h1>Profile</h1>
+      <h2 className="p-1 rounded bg-green-500">
+        {data ? <Link href={`/profile/${data}`}>{data}</Link> : "Nothing"}
+      </h2>
+      <hr />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={logout}
+      >
+        Log out
+      </button>
+
+      <button
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={getUserDetails}
+      >
+        Get Details
+      </button>
     </div>
   );
 }
